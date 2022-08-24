8eb9126b318279c4a8e2fcb7b85e7bcb5a207929
usagiga <usagiga@usagiga.me>
2022-08-25 06:39:33 +0900

    feat: define Portrait component(WIP)

---
+++ src/components/Portrait.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Portrait.css';
+
+const Portrait: React.FC = () => {
+    return (
+        <div className='Portrait'>
+            <img src='https://usagiga.me/assets/image/logo.png' alt="portrait" />
+        </div>
+    );
+};
+
+export default Portrait;
