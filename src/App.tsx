5c066c990d5dae5ca8009d6d1c752dfac4be7b3b
usagiga <usagiga@usagiga.me>
2022-09-04 03:58:50 +0900

    feat: add job and batches

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,31 @@ function App() {
       <header className="appHeader">
         <Portrait />
         <p className="name">usagiga</p>
+        <p className="batches">🎀💉</p>
+        <p className="job">
+          <ul>
+            <li>
+              Software engineer(Go, ASP.NET Core, React)
+              <ul>
+                <li>DeNA Co., Ltd. (2021-)</li>
+              </ul>
+            </li>
+            <li>
+              Professional Cloud Developer
+              <ul>
+                <li>E.G.G. Japan 5th Attendee</li>
+              </ul>
+            </li>
+            <li>
+              AUR package co-maintainer(
+              <a href="https://aur.archlinux.org/packages/anyenv">anyenv</a>)
+            </li>
+            <li>
+              Founder of{' '}
+              <a href="https://github.com/snowbelle-org">snowbelle.org</a>
+            </li>
+          </ul>
+        </p>
       </header>
     </div>
   );
