ea0b07161a3636fa530418d7c0e9d3484a17cc83
usagiga <usagiga@usagiga.me>
2022-09-12 05:15:58 +0900

    fix: ul tag shouldn't be appeared inside p tag

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,32 @@ function App() {
         <Portrait />
         <p className={styles.name}>usagiga</p>
         <p className={styles.batches}>🎀💉</p>
-        <p className={styles.job}>
-          <ul>
-            <li>
-              Software engineer(Go, ASP.NET Core, React)
-              <ul>
-                <li>DeNA Co., Ltd. (2021-)</li>
-              </ul>
-            </li>
-            <li>
-              Professional Cloud Developer
-              <ul>
-                <li>E.G.G. Japan 5th Attendee</li>
-              </ul>
-            </li>
-            <li>
-              AUR package co-maintainer(
-              <a href="https://aur.archlinux.org/packages/anyenv">anyenv</a>)
-            </li>
-            <li>
-              Founder of{' '}
-              <a href="https://github.com/snowbelle-org">snowbelle.org</a>
-            </li>
-          </ul>
-        </p>
+        <ul className={styles.job}>
+          <li>
+            Software engineer(Go, ASP.NET Core, React)
+            <ul>
+              <li>DeNA Co., Ltd. (2021-)</li>
+            </ul>
+          </li>
+          <li>
+            Professional Cloud Developer
+            <ul>
+              <li>E.G.G. Japan 5th Attendee</li>
+            </ul>
+          </li>
+          <li>
+            AUR package maintainer
+            <ul>
+              <li>
+                <a href="https://aur.archlinux.org/packages/anyenv">anyenv</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            Founder of{' '}
+            <a href="https://github.com/snowbelle-org">snowbelle.org</a>
+          </li>
+        </ul>
       </header>
     </div>
   );
