91a04458187b456c4e4e16919365319845cbcc85
usagiga <usagiga@usagiga.me>
2022-09-24 01:49:08 +0900

    feat: remove batches

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
       <header className={styles.appHeader}>
         <Portrait />
         <p className={styles.name}>usagiga</p>
-        <p className={styles.batches}>🎀💉</p>
         <ul className={styles.job}>
           <li>
             Software engineer(Go, ASP.NET Core, React)
