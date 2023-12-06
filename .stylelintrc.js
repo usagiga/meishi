419d776286ccb67018e5e327e753fefb6dad0471
usagiga <usagiga@usagiga.me>
2023-12-07 00:55:03 +0900

    chore: enable scss in stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -11,5 +11,7 @@ module.exports = {
     'string-quotes': 'single',
     'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
     'keyframes-name-pattern': '^[a-z][a-zA-Z0-9]+$',
+    "function-no-unknown": null,
+    "scss/function-no-unknown": true
   },
 };
